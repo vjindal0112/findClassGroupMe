368fbd387365757d37b82b4795d1f76903177550
formans <formans@umich.edu>
2020-11-06 10:31:12 -0500

    searchbar first version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import SelectBar from "./components/SearchBar";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <SelectBar
+          title="Enter your classes."
+          label="Class"
+          keyName={"Class"}
+          choices={options}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +30,11 @@ function App() {
 }
 
 export default App;
+
+const options = [
+  { value: "AERO ST 1A - Liscombe", label: "AERO ST 1A - Liscombe" },
+  { value: "AERO ST 20A - Liscombe", label: "AERO ST 20A - Liscombe" },
+  { value: "AERO ST 130A - Jones", label: "AERO ST 130A - Jones" },
+  { value: "AERO ST 140A - Fowler", label: "AERO ST 140A - Fowler" },
+  { value: "AF AMER 1 - Pierre", label: "AF AMER 1 - Pierre" },
+];
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,50 @@
-import React from 'react';
+import React, { useEffect } from "react";
+
+import Select from "react-select";
+
+const SelectBar = ({
+  title,
+  label,
+  keyName,
+  choices,
+  moveSectionDown,
+  onChange,
+  initial,
+  reset,
+}) => {
+  useEffect(() => {}, [keyName, choices, initial, reset]);
+
+  return (
+    <div className="section">
+      <Select
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            moveSectionDown();
+          }
+        }}
+        placeholder={label}
+        key={reset ? `${keyName}_${reset}` : keyName}
+        options={choices}
+        inputValue={initial}
+        onInputChange={(e) => {
+          console.log("onInputChange");
+          console.log(e);
+          if (e !== "" && typeof e != "object") {
+            onChange(keyName, e);
+          } else if (initial && initial.length === 1 && typeof e != "object") {
+            onChange(keyName, e);
+          }
+        }}
+        onChange={(newValue, actionMeta) => {
+          console.log("onInputChange");
+          console.log(newValue);
+          if (actionMeta.action === "select-option") {
+            onChange(keyName, newValue.value);
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default SelectBar;
