71d462e32efaf621ca021f832366c2c62b57ee8a
formans <formans@umich.edu>
2020-11-10 01:30:08 -0500

    search bar styling and final functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useState, onChange } from "react";
 import styled from "styled-components";
 
 const options = [
-  { value: "AERO ST 1A - Liscombe", label: "AERO ST 1A - Liscombe" },
+  { value: "AERO ST 1A - Liscombe", label: "aero st 1a - liscombe" },
   { value: "AERO ST 20A - Liscombe", label: "AERO ST 20A - Liscombe" },
   { value: "AERO ST 130A - Jones", label: "AERO ST 130A - Jones" },
   { value: "AERO ST 140A - Fowler", label: "AERO ST 140A - Fowler" },
@@ -11,10 +11,17 @@ const options = [
 
 const SelectDiv = styled.div`
   width: 300px;
-  height: 300px;
-  top: 50%;
+  height: 120px;
+  top: 10%;
   left: 40%;
   position: absolute;
+  maxheight: 120px;
+  overflow: scroll;
+`;
+
+const Bar = styled.div`
+  top: 46%;
+  position: center;
 `;
 
 const SelectBar = () => {
@@ -22,14 +29,30 @@ const SelectBar = () => {
 
   return (
     <>
-      <input
-        type="input"
-        placeholder="Search..."
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      {options.map((obj, index) => (
-        <>{obj.value.includes(query) ? <p>{obj.value}</p> : null}</>
-      ))}
+      <Bar>
+        <input
+          style={{
+            width: "600px",
+            height: "49px",
+            fontSize: "18px",
+            borderWidth: "2px",
+            borderColor: "#05AFF0",
+            paddingLeft: "12px",
+          }}
+          type="input"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </Bar>
+      <SelectDiv>
+        {options.map((obj, index) => (
+          <>
+            {obj.value.toLowerCase().includes(query.toLowerCase()) ? (
+              <p>{obj.value}</p>
+            ) : null}
+          </>
+        ))}
+      </SelectDiv>
     </>
   );
 };
