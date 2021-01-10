6b5a33ddf0fd38b2316ffd4cfca73bb9f6d4fe2e
formans <formans@umich.edu>
2021-01-09 22:28:06 -0500

    added classes

--- src/amplitude.js
+++ src/amplitude.js
@@ -1,18 +1,18 @@
-import amplitude from 'amplitude-js';
+import amplitude from "amplitude-js";
 
 export const initAmplitude = () => {
   amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE);
 };
 
-export const setAmplitudeUserDevice = installationToken => {
+export const setAmplitudeUserDevice = (installationToken) => {
   amplitude.getInstance().setDeviceId(installationToken);
 };
 
-export const setAmplitudeUserId = userId => {
+export const setAmplitudeUserId = (userId) => {
   amplitude.getInstance().setUserId(userId);
 };
 
-export const setAmplitudeUserProperties = properties => {
+export const setAmplitudeUserProperties = (properties) => {
   amplitude.getInstance().setUserProperties(properties);
 };
 
